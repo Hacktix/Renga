0dab2ff66fa4067629d96ac49fb62bbe5e1e7d66
Hacktix <zecmad16@htlkaindorf.at>
2022-12-18 15:36:07 +0100

    implement basic settings system

---
+++ Config/RengaConfig.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+
+namespace Renga.Config
+{
+    internal class RengaConfig
+    {
+        private Dictionary<string, object> _config;
+
+        public RengaConfig()
+        {
+            try
+            {
+                string confText = File.ReadAllText("settings.json");
+                _config = JsonConvert.DeserializeObject<Dictionary<string, object>>(confText);
+            }
+            catch(FileNotFoundException e)
+            {
+                _config = GenerateDefaultSettings();
+            }
+        }
+
+        private static Dictionary<string, object> GenerateDefaultSettings()
+        {
+            string confText = Properties.Resources.DefaultConfig;
+            File.WriteAllText("settings.json", confText);
+            return JsonConvert.DeserializeObject<Dictionary<string, object>>(confText);
+        }
+
+        public T GetProperty<T>(string key)
+        {
+            return (T)_config[key];
+        }
+    }
+}
--- Renga.cs
+++ Renga.cs
@@ -1,9 +1,12 @@
 ﻿using Chroma;
+using Renga.Config;
 
 namespace Renga
 {
     public class Renga : Game
     {
+        private RengaConfig _conf = new RengaConfig();
+
         public Renga() : base(new(true, false))
         {
             Window.Title = "れんが";
