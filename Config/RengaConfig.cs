46cfa1c350e3595346b040d1b84e0b82ee3b80d9
Hacktix <zecmad16@htlkaindorf.at>
2022-12-26 18:33:03 +0100

    fix config json path

--- Config/RengaConfig.cs
+++ Config/RengaConfig.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json;
+using System.Reflection;
 
 #pragma warning disable CS8601 // Possible null reference assignment.
 #pragma warning disable CS8603 // Possible null reference return.
+#pragma warning disable CS8604 // Possible null reference argument.
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 namespace Renga.Config
@@ -9,12 +11,13 @@ namespace Renga.Config
     public class RengaConfig
     {
         private Dictionary<string, object> _config;
+        private static readonly string ConfigPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "settings.json");
 
         public RengaConfig()
         {
             try
             {
-                string confText = File.ReadAllText("settings.json");
+                string confText = File.ReadAllText(ConfigPath);
                 _config = JsonConvert.DeserializeObject<Dictionary<string, object>>(confText);
             }
             catch(FileNotFoundException)
@@ -26,7 +29,7 @@ namespace Renga.Config
         private static Dictionary<string, object> GenerateDefaultSettings()
         {
             string confText = Properties.Resources.DefaultConfig;
-            File.WriteAllText("settings.json", confText);
+            File.WriteAllText(ConfigPath, confText);
             return JsonConvert.DeserializeObject<Dictionary<string, object>>(confText);
         }
 
@@ -53,7 +56,7 @@ namespace Renga.Config
         private void SaveSettings()
         {
             string confText = JsonConvert.SerializeObject(_config);
-            File.WriteAllText("settings.json", confText);
+            File.WriteAllText(ConfigPath, confText);
         }
     }
 }
